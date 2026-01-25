4986d282c430739dcd29bb06ac21e1d3079148d9
Anthaguz <anthonyag7@hotmail.com>
2026-01-24 22:33:12 -0600

    feat: add dry-run deployment pipeline

---
+++ LabAssistant.Business/Deployment/DryRunDeploymentResult.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using LabAssistant.Models.Deployment;
+using LabAssistant.Models.Templates;
+
+namespace LabAssistant.Business.Deployment;
+
+public class DryRunDeploymentResult
+{
+    public DeploymentPlan Plan { get; }
+    public List<string> Logs { get; } = new();
+
+    public DryRunDeploymentResult(DeploymentPlan plan)
+    {
+        Plan = plan;
+    }
+}
