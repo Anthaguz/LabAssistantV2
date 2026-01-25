2b3d3b8032e3f51e6b70a8cbf5acdb2ec472fab9
Anthaguz <anthonyag7@hotmail.com>
2026-01-24 21:21:11 -0600

    feat: add template loader

---
+++ LabAssistant.Models/Templates/LabTemplateLoadResult.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using LabAssistant.Models.Templates;
+
+namespace LabAssistant.Models.Templates;
+
+public class LabTemplateLoadResult
+{
+    public List<LabTemplate> Templates { get; } = new();
+    public List<string> Errors { get; } = new();
+
+    public bool IsValid => Errors.Count == 0;
+}
