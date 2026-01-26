13d07d91d9d7c2b7a2bd30faaad16b09e058bce2
Anthaguz <anthonyag7@hotmail.com>
2026-01-25 21:39:32 -0600

    Add template editor page shell

---
+++ LabAssistant/Views/TemplateEditorPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace LabAssistant.Views
+{
+    public partial class TemplateEditorPage : Page
+    {
+        public TemplateEditorPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
