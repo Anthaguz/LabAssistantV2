875f4a0e15f4ec9729b4095232dd05611dba0e94
Anthaguz <anthonyag7@hotmail.com>
2026-02-21 16:44:26 -0600

    test: update virtual switch provider namespace reference

--- LabAssistant.Business.Tests/Tests/VirtualSwitchProviderTests.cs
+++ LabAssistant.Business.Tests/Tests/VirtualSwitchProviderTests.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using LabAssistant.Business;
+using LabAssistant.Business.Deployment;
 using LabAssistant.Services.HyperV;
 using LabAssistant.Services.PowerShell;
 using Xunit;
