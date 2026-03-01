2f6758ff18bf082c0455a1b5797ce375217da943
Anthaguz <anthonyag7@hotmail.com>
2026-03-01 15:06:36 -0600

    refactor: decompose winui machines and diagnostics views

---
+++ LabAssistant.WinUI/Views/Machines/MachinesOverviewView.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace LabAssistant.WinUI.Views.Machines;
+
+public sealed partial class MachinesOverviewView : UserControl
+{
+    public MachinesOverviewView()
+    {
+        InitializeComponent();
+    }
+}
